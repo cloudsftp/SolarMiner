caa5b301049b884713655d005edc04f8d00141ea
Fabian Weik <fabian.weik@protonmail.com>
2025-05-09 21:59:11 +0200

    rough sketch of controller software architecture

--- Pi/controller/src/main.rs
+++ Pi/controller/src/main.rs
@@ -1,9 +1,69 @@
 use anyhow::Error;
 use dotenv::dotenv;
-use rumqttc::{AsyncClient, Event, MqttOptions, Packet, QoS};
-use std::{env, time::Duration};
+use rumqttc::{AsyncClient, Event, EventLoop, MqttOptions, Packet, Publish, QoS};
+use std::{env, io, time::Duration};
 
 mod plug;
+
+enum PowerState {
+    ON,
+    OFF,
+    Unknown,
+}
+
+struct State {
+    plug: PowerState,
+}
+
+enum UpdateEvent {
+    PlugUpdate { device: String, on: bool },
+}
+
+impl TryFrom<Publish> for UpdateEvent {
+    type Error = Error;
+
+    fn try_from(value: Publish) -> Result<Self, Self::Error> {
+        let device = value.topic; // TODO: string parsing :(
+        todo!()
+    }
+}
+
+impl State {
+    async fn run(mut eventloop: EventLoop) {
+        let mut state = Self {
+            plug: PowerState::Unknown,
+        };
+
+        loop {
+            let event = eventloop.poll().await.expect("for now, panic"); // TODO: remove panic
+            state = match state.handle_event(event).await.expect("for now, panic") {
+                Some(state) => state,
+                None => break,
+            };
+        }
+    }
+
+    async fn handle_event(self, event: Event) -> Result<Option<Self>, Error> {
+        Ok(match event {
+            Event::Incoming(Packet::Publish(publish)) => {
+                let decoded = UpdateEvent::try_from(publish)?;
+                /*
+                println!(
+                    "Received = '{}' on topic '{}'",
+                    String::from_utf8_lossy(&publish.payload),
+                    publish.topic
+                );
+                 */
+                Some(self)
+            }
+            Event::Incoming(Packet::Disconnect) => {
+                println!("Disconnected");
+                None
+            }
+            _ => Some(self),
+        })
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -13,51 +73,31 @@ async fn main() -> Result<(), Error> {
     mqttoptions.set_credentials(env::var("MQTT_USER")?, env::var("MQTT_PASSWORD")?);
     mqttoptions.set_keep_alive(Duration::from_secs(5));
 
-    let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+    let (client, eventloop) = AsyncClient::new(mqttoptions, 10);
 
     let client_sub = client.clone();
     tokio::spawn(async move {
         client_sub
-            .subscribe("stat/plug_bitaxe_001/RESULT", QoS::AtLeastOnce)
+            .subscribe("stat/+/RESULT", QoS::AtLeastOnce)
             .await
             .unwrap();
         println!("Subscribed to stat/plug_bitaxe_001/RESULT");
 
-        loop {
-            match eventloop.poll().await {
-                Ok(Event::Incoming(Packet::Publish(publish))) => {
-                    println!(
-                        "Received = '{}' on topic '{}'",
-                        String::from_utf8_lossy(&publish.payload),
-                        publish.topic
-                    );
-                }
-                Ok(Event::Incoming(Packet::Disconnect)) => {
-                    println!("Disconnected");
-                    break;
-                }
-                Err(e) => {
-                    eprintln!("Error in eventloop: {:?}", e);
-                    tokio::time::sleep(Duration::from_secs(1)).await;
-                }
-                _ => {}
-            }
-        }
+        State::run(eventloop).await;
     });
 
-    tokio::time::sleep(Duration::from_secs(1)).await;
+    loop {
+        println!("press button to toggle plug");
+        let _ = io::stdin().read_line(&mut String::new());
 
-    client
-        .publish(
-            "cmnd/plug_bitaxe_001/Power",
-            QoS::AtLeastOnce,
-            false,
-            "TOGGLE",
-        )
-        .await?;
-    println!("Published message.");
-
-    tokio::time::sleep(Duration::from_secs(5)).await;
-
-    Ok(())
+        client
+            .publish(
+                "cmnd/plug_bitaxe_001/Power",
+                QoS::AtLeastOnce,
+                false,
+                "TOGGLE",
+            )
+            .await?;
+        println!("Published message.");
+    }
 }
