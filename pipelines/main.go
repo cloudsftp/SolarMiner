24edfa9fbd439e9d624fe4fa223d10accf8a7833
Fabian Weik <fabian.weik@protonmail.com>
2025-05-26 11:20:30 +0200

    slim dependencies needed for cross compiling

--- pipelines/main.go
+++ pipelines/main.go
@@ -1,17 +1,3 @@
-// A generated module for SolarMiner functions
-//
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
-
 package main
 
 import (
@@ -126,17 +112,15 @@ func (b *SolarMiner) CrossCompileController(
 	return dag.Container().
 		From("rust:"+RustVersion+"-alpine"+AlpineVersion).
 
-		// Openssl
+		// Clang
 		WithExec([]string{"apk", "update"}).
 		WithExec([]string{
 			"apk", "add", "--no-cache",
-			"pkgconfig", "musl-dev",
-			"openssl-dev", "openssl-libs-static",
-			"clang", "lld", "perl", "make",
+			//"musl-dev",
+			"clang", "lld",
 		}).
 
-		// Clippy
-		//WithExec([]string{"rustup", "component", "add", "clippy"}).
+		// Target
 		WithExec([]string{"rustup", "target", "add", "armv7-unknown-linux-musleabihf"}).
 
 		// Caches
