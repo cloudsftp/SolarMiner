dc3c8eb1a9b61db15d57590c8e7061344f8e275d
Fabian Weik <fabian.weik@protonmail.com>
2025-05-28 13:05:26 +0200

    split decoding nats messages into two parts

--- pi/src/state/update/events/mod.rs
+++ pi/src/state/update/events/mod.rs
@@ -5,7 +5,6 @@ use anyhow::{Context as AnyhowContext, Error};
 use async_nats::{Message, Subject};
 use bytes::Bytes;
 use itertools::Itertools;
-use log::debug;
 use serde::Deserialize;
 use serde_repr::{Deserialize_repr, Serialize_repr};
 
@@ -103,11 +102,15 @@ struct BatteryState {
 #[derive(Debug, PartialEq, Serialize_repr, Deserialize_repr)]
 #[repr(usize)]
 enum BatteryStatus {
+    Standby = 1,
+    Unknown2 = 2,
     Charging = 3,
     Discharging = 4,
     Fault = 5,
     PreservingCharge = 6,
     Idle = 7,
+    Unknown8 = 8,
+    Unknown9 = 9,
     PowerSaving = 10,
 }
 
@@ -115,78 +118,100 @@ impl TryFrom<&Message> for UpdateEvent {
     type Error = Error;
 
     fn try_from(message: &Message) -> Result<Self, Self::Error> {
-        let device_parts = message.subject.split(".").collect_vec();
+        let subject_parts = message.subject.split(".").collect_vec();
 
-        Ok(match device_parts.as_slice() {
-            // Future: maybe use location
-            ["stat", _location @ .., device, "RESULT"] => {
-                let device = (*device).into();
-
-                let result: CommandResult =
-                    serde_json::from_slice(&message.payload).context(format!(
-                        "could not decode payload '{}' received on subject '{}'",
-                        String::from_utf8_lossy(&message.payload),
-                        message.subject,
-                    ))?;
-
-                match result {
-                    CommandResult::Power(value) => {
-                        let on = matches!(value, PlugStateValue::On);
-                        UpdateEvent::PlugStateUpdate { device, on }
-                    }
-                    CommandResult::EnergyConsumption {
-                        total,
-                        yesterday,
-                        today,
-                    } => UpdateEvent::PlugEnergyUpdate {
-                        device: device.to_string(),
-                        total,
-                        yesterday,
-                        today,
-                    },
-                }
-            }
-            ["stat", _location @ .., device, "POWER"] => {
-                let plug_update: PlugStateValue = serde_plain::from_str(&String::from_utf8_lossy(
-                    &message.payload,
-                ))
-                .context(format!(
-                    "could not decode payload '{}' received on subject '{}'",
-                    String::from_utf8_lossy(&message.payload),
-                    message.subject,
-                ))?;
-
-                let device = (*device).into();
-                let on = matches!(plug_update, PlugStateValue::On);
-                UpdateEvent::PlugStateUpdate { device, on }
-            }
-            ["solaredge", "powerflow"] => {
-                let power_flow: Power = serde_json::from_slice(&message.payload)?;
-
-                UpdateEvent::PowerUpdate {
-                    pv_production: power_flow.pv_production,
-                    house_demand: power_flow.consumer.house,
-                    grid: PowerDemand {
-                        demand: power_flow.grid.consumption,
-                        production: power_flow.grid.delivery,
-                    },
-                    battery: PowerDemand {
-                        demand: power_flow.battery.charge,
-                        production: power_flow.battery.discharge,
-                    },
-                }
-            }
-            ["solaredge", "modbus", "battery", _battery_name] => {
-                let battery_state: BatteryState = serde_json::from_slice(&message.payload)?;
-
-                UpdateEvent::BatteryUpdate {
-                    level: battery_state.state_of_charge,
-                }
-            }
+        Ok(match subject_parts.as_slice() {
+            ["stat", subject_parts @ ..] => decode_plug_message(subject_parts, message)?,
+            ["solaredge", subject_parts @ ..] => decode_solaredge_message(subject_parts, message)?,
             _ => UpdateEvent::Unknown {
                 subject: message.subject.clone(),
                 payload: message.payload.clone(),
             },
         })
     }
+}
+
+fn decode_plug_message(topic_parts: &[&str], message: &Message) -> Result<UpdateEvent, Error> {
+    Ok(match topic_parts {
+        [_location @ .., device, "RESULT"] => {
+            let device = (*device).into();
+
+            let result: CommandResult =
+                serde_json::from_slice(&message.payload).context(format!(
+                    "could not decode payload '{}' received on subject '{}'",
+                    String::from_utf8_lossy(&message.payload),
+                    message.subject,
+                ))?;
+
+            match result {
+                CommandResult::Power(value) => {
+                    let on = matches!(value, PlugStateValue::On);
+                    UpdateEvent::PlugStateUpdate { device, on }
+                }
+                CommandResult::EnergyConsumption {
+                    total,
+                    yesterday,
+                    today,
+                } => UpdateEvent::PlugEnergyUpdate {
+                    device: device.to_string(),
+                    total,
+                    yesterday,
+                    today,
+                },
+            }
+        }
+        [_location @ .., device, "POWER"] => {
+            let plug_update: PlugStateValue = serde_plain::from_str(&String::from_utf8_lossy(
+                &message.payload,
+            ))
+            .context(format!(
+                "could not decode payload '{}' received on subject '{}'",
+                String::from_utf8_lossy(&message.payload),
+                message.subject,
+            ))?;
+
+            let device = (*device).into();
+            let on = matches!(plug_update, PlugStateValue::On);
+            UpdateEvent::PlugStateUpdate { device, on }
+        }
+        _ => UpdateEvent::Unknown {
+            subject: message.subject.clone(),
+            payload: message.payload.clone(),
+        },
+    })
+}
+
+fn decode_solaredge_message(
+    subject_parts: &[&str],
+    message: &Message,
+) -> Result<UpdateEvent, Error> {
+    Ok(match subject_parts {
+        ["powerflow"] => {
+            let power_flow: Power = serde_json::from_slice(&message.payload)?;
+
+            UpdateEvent::PowerUpdate {
+                pv_production: power_flow.pv_production,
+                house_demand: power_flow.consumer.house,
+                grid: PowerDemand {
+                    demand: power_flow.grid.consumption,
+                    production: power_flow.grid.delivery,
+                },
+                battery: PowerDemand {
+                    demand: power_flow.battery.charge,
+                    production: power_flow.battery.discharge,
+                },
+            }
+        }
+        ["modbus", "battery", _battery_name] => {
+            let battery_state: BatteryState = serde_json::from_slice(&message.payload)?;
+
+            UpdateEvent::BatteryUpdate {
+                level: battery_state.state_of_charge,
+            }
+        }
+        _ => UpdateEvent::Unknown {
+            subject: message.subject.clone(),
+            payload: message.payload.clone(),
+        },
+    })
 }
